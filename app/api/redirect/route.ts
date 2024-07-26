8f38fcf055a5a4470875e33bb4101f8e503dbd78
Walid <w.cawasa@gmail.com>
2024-07-27 01:28:29 +0800

    :hammer_and_wrench: refactor: added dynamic origin on SOA

--- app/api/redirect/route.ts
+++ app/api/redirect/route.ts
@@ -1,26 +1,38 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-export async function GET(req:any) {
+export async function GET(req: any) {
   const krParam = req.nextUrl.searchParams.get("kr");
   const productTitle = req.nextUrl.searchParams.get("title");
+  const mode = req.nextUrl.searchParams.get("mode") || ""; // THIS IS FOR EXTENSIONS SOA, SOE not on ebextractor
+  const origin = req.nextUrl.searchParams.get("origin") || "";
   const kr: string | undefined = process.env.KR || undefined;
   const tag: string | undefined = process.env.TAG || undefined;
-  if (kr === krParam && productTitle && tag!==undefined) {
-    console.log("redirecting")
+  if (kr === krParam && productTitle && tag !== undefined) {
+    console.log("redirecting");
     return new NextResponse(null, {
       status: 302,
       headers: {
-        Location: `https://www.amazon.com/s?k=${encodeURIComponent(productTitle)}${tag}`
-      }
+        Location:
+          mode !== "SOA" && origin === ""
+            ? `https://www.amazon.com/s?k=${encodeURIComponent(
+                productTitle
+              )}${tag}`
+            : `${origin}/s?k=${encodeURIComponent(productTitle)}${tag}`,
+      },
     });
   } else {
-    console.log("ENV NOT FOUND --- redirecting")
+    console.log("ENV NOT FOUND --- redirecting");
     return new NextResponse(JSON.stringify({ data: [] }), {
       status: 302,
       headers: {
-        'Content-Type': 'application/json',
-        Location: `https://www.amazon.com/s?k=${encodeURIComponent(productTitle)}${tag}`
-      }
+        "Content-Type": "application/json",
+        Location:
+          mode !== "SOA" && origin === ""
+            ? `https://www.amazon.com/s?k=${encodeURIComponent(
+                productTitle
+              )}${tag}`
+            : `${origin}/s?k=${encodeURIComponent(productTitle)}${tag}`,
+      },
     });
   }
 }
